6fef6354aefcbf955306b7c54bcc0a97d43a1ed7
sgh1827 <81658308+sgh1827@users.noreply.github.com>
2021-04-04 22:27:33 -0500

    Update q1.py

--- hw2/q1.py
+++ hw2/q1.py
@@ -4,7 +4,46 @@ import csv
 data1 = [] # column 1
 data2 = [] # column 2
 
+with open('sigA.csv') as f:
+    # open the csv file
+    reader = csv.reader(f)
+    for row in reader:
+        # read the rows 1 one by one
+        #t.append(float(row[0])) # leftmost column
+        data1.append(float(row[0])) # second column
+        data2.append(float(row[1])) # third column
+        
+for i in range(len(data1)):
+    # print the data to verify it was read
+    print(str(data1[i]) + ", " + str(data2[i]))
+        
+with open('sigB.csv') as f:
+    # open the csv file
+    reader = csv.reader(f)
+    for row in reader:
+        # read the rows 1 one by one
+        #t.append(float(row[0])) # leftmost column
+        data1.append(float(row[0])) # second column
+        data2.append(float(row[1])) # third column
+        
+for i in range(len(data1)):
+    # print the data to verify it was read
+    print(str(data1[i]) + ", " + str(data2[i]))        
+
 with open('sigC.csv') as f:
+    # open the csv file
+    reader = csv.reader(f)
+    for row in reader:
+        # read the rows 1 one by one
+        #t.append(float(row[0])) # leftmost column
+        data1.append(float(row[0])) # second column
+        data2.append(float(row[1])) # third column  
+        
+for i in range(len(data1)):
+    # print the data to verify it was read
+    print(str(data1[i]) + ", " + str(data2[i]))        
+        
+with open('sigD.csv') as f:
     # open the csv file
     reader = csv.reader(f)
     for row in reader:
